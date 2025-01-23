a3fa9b9cb4443c371d20a2f0f8bd60c6547598d1
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-23 18:52:44 +0700

    Add: configure settings for production with environment variables and update WSGI application

--- MAP/settings.py
+++ MAP/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^q^qh#b*^5x(w%!54&qsmb%9(ohkduz2+!_)8q-=%m6!!h1adc'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.vercel.app',
+    '.ridwaanhall.me',
+]
 
 
 # Application definition
