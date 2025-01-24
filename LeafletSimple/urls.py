a151f1050eb705cc569165515e8dba2f0250d648
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-24 16:08:49 +0700

    feat: extend views and add new templates for zoom levels tutorial

--- LeafletSimple/urls.py
+++ LeafletSimple/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path(f'{view}/', getattr(views, f'Leaflet{view.upper()}View').as_view(), name=f'leaflet_{view}')
-    for view in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
+    for view in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
 ]
--- LeafletSimple/views.py
+++ LeafletSimple/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import TemplateView
 
-views = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
+views = [
+    'a', 'b', 'c', 'd', 'e', 
+    'f', 'g', 'h', 'i', 'j', 
+    'k', 'l', 'm', 'n', 'o',
+    'p',
+]
 
 for view in views:
     globals()[f'Leaflet{view.upper()}View'] = type(
